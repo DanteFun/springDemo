d92f8cf999606d35df7b54c18a30a89290acc24d
DanteFun <1610671801@qq.com>
2020-11-08 18:56:50 +0800

    Spring mvc test pass!

--- SpringTest/src/com/HelloAction.java
+++ SpringTest/src/com/HelloAction.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.mvc.Controller;
 
 public class HelloAction implements Controller {
@@ -12,9 +13,8 @@ public class HelloAction implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("/hello.jsp");
+		modelAndView.setViewName("hello");
 		
 		return modelAndView;
 	}
-
 }
